fb93cdd29f78804fa695fab5edf86394411efe39
MatteoGaloni <mtgaloni@gmail.com>
2023-04-18 17:10:12 +0200

    add else (accesso negato)

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ let person = prompt("Per favore inserisci la tua email")
 // ***apro ciclo for per richiamare i valori dell'array***
 for (let c = 0; c < emailUsers.length; c++) {
 // ***creo variabile che conterrà gli users dell'array***
+    
     const user = emailUsers[c];
+    
 // ***creo condizione per stampare hai vinto****
     if (user == person) {
-        console.log("hai vinto")
-    }
+        console.log("Credenziali valide")
+        alert("Credenziali valide");
+    } else alert("Accesso negato");
    
 }
+
