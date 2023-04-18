f407da7009e0106bf0adf4b6b71b5dfbc6f40819
MatteoGaloni <mtgaloni@gmail.com>
2023-04-18 15:38:27 +0200

    add IF to select the right email

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ let person = prompt("Per favore inserisci la tua email")
 
 // ***apro ciclo for per richiamare i valori dell'array***
 for (let c = 0; c < emailUsers.length; c++) {
-     console.log( emailUsers[c] );
+    console.log(emailUsers[c]);
+    
+// ***condizione: se la mail inserita è quella di flavia allora hai vinto***  
+    if (c = "flavia@gmail.com") {
+        console.log("Hai vinto");
+    }
 
     
 }
