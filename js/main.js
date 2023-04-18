144d5704f3c8387873a9a306549b2ae5c3d71ac3
MatteoGaloni <mtgaloni@gmail.com>
2023-04-18 15:34:25 +0200

    add array and for

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// ****aggiungo emailUsers array e creo prompt****
+const emailUsers = ["flavia@gmail.com", "marco@gmail.com"]
+
+let person = prompt("Per favore inserisci la tua email")
+
+// ***apro ciclo for per richiamare i valori dell'array***
+for (let c = 0; c < emailUsers.length; c++) {
+     console.log( emailUsers[c] );
+
+    
+}
